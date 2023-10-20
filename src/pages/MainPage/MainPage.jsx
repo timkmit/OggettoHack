e7bf32e36c55f712edf3ce734f2ef75b64b80504
Timur <timengalytchev@mail.ru>
2023-10-21 00:47:52 +0300

    main page without meetings

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,10 @@
 import '../MainPage/style.css'
 import imgLogoMobile from "../../img/oggetto-flat-logo.png";
 import imgLogoDesktop from "../../img/oggetto-logo_tonal-hor-rus.png";
+import imgLogoDesktopBack from "../../img/oggetto-flat-logo-back.png";
 import * as colors from '../../img/colors.jsx'
 import {Button} from '../../components/Button';
-
+//TODO: сделать митинги в теле боди, добавить футер с инфой как у них
 function MainPage() {
     return ( 
         <>
@@ -27,8 +28,20 @@ function MainPage() {
     <Button text="Регистрация"/>
       </div>
     </header>
+
+    <body style={bodyStyle}>
+        <h1 style={{paddingLeft: '8%', paddingRight: '25%', paddingTop: '4%', fontSize:'66px'}}>Твоя платформа для проведения well-being мероприятий</h1>
+        
+    </body>
         </>
      );
+}
+
+const bodyStyle = {
+    paddingTop: '2rem',
+    backgroundImage: `url(${imgLogoDesktopBack})`,
+    backgroundSize: 'cover',
+    height: '1000px'
 }
 
 const headerStyle = {
@@ -37,8 +50,8 @@ const headerStyle = {
     alignItems: 'center',
     backgroundColor: colors.WHITE,
     color: '#fff',
-    marginLeft: '10%',
-    marginRight: '10%',
+    marginLeft: '8%',
+    marginRight: '8%',
   };
   
   const logoStyle = {
