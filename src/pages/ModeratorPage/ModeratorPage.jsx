727a227b0b0b9c4f82156f9e73d0e5f0262fccfe
Timur <timengalytchev@mail.ru>
2023-10-21 04:33:16 +0300

    moder page 40%

--- src/pages/ModeratorPage/ModeratorPage.jsx
+++ src/pages/ModeratorPage/ModeratorPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../ModeratorPage/style.css'
 
@@ -18,19 +18,35 @@ qa: [{id: 1, text: "blablalbla"}, {id:2, text: "damn so cool"}], feedback: [ {id
 qa: [{id: 1, text: "blablalbla"}, {id:2, text: "damn so cool"}], feedback: [ {id: 1, text:"kirill"}, {id:2, text:"danya"}] },
 {id: 4, name: "Созвон с пиццой", actual: true, author: "Витя Матильдов", date: "21.10.2023", topic: "Пиццаводство", materials: "url", 
 qa: [{id: 1, text: "blablalbla"}, {id:2, text: "damn so cool"}], feedback: [ {id: 1, text:"kirill"}, {id:2, text:"danya"}] },
-{id: 1, name: "Созвон с чиксами", actual: true, author: "Вика Матильдова", date: "21.10.2023", topic: "Животноводство", materials: "url", 
+{id: 5, name: "Созвон с чиксами", actual: true, author: "Вика Матильдова", date: "21.10.2023", topic: "Животноводство", materials: "url", 
 qa: [{id: 1, text: "blablalbla"}, {id:2, text: "damn so cool"}], feedback: [ {id: 1, text:"kirill"}, {id:2, text:"danya"}] },
-{id: 2, name: "Созвон с пиццой", actual: true, author: "Витя Матильдов", date: "21.10.2023", topic: "Пиццаводство", materials: "url", 
+{id: 6, name: "Созвон с пиццой", actual: true, author: "Витя Матильдов", date: "21.10.2023", topic: "Пиццаводство", materials: "url", 
 qa: [{id: 1, text: "blablalbla"}, {id:2, text: "damn so cool"}], feedback: [ {id: 1, text:"kirill"}, {id:2, text:"danya"}] },
-{id: 3, name: "Созвон с чиксами", actual: true, author: "Вика Матильдова", date: "21.10.2023", topic: "Животноводство", materials: "url", 
+{id: 7, name: "Созвон с чиксами", actual: true, author: "Вика Матильдова", date: "21.10.2023", topic: "Животноводство", materials: "url", 
 qa: [{id: 1, text: "blablalbla"}, {id:2, text: "damn so cool"}], feedback: [ {id: 1, text:"kirill"}, {id:2, text:"danya"}] },
-{id: 4, name: "Созвон с пиццой", actual: true, author: "Витя Матильдов", date: "21.10.2023", topic: "Пиццаводство", materials: "url", 
+{id: 8, name: "Созвон с пиццой", actual: true, author: "Витя Матильдов", date: "21.10.2023", topic: "Пиццаводство", materials: "url", 
+qa: [{id: 1, text: "blablalbla"}, {id:2, text: "damn so cool"}], feedback: [ {id: 1, text:"kirill"}, {id:2, text:"danya"}] },
+{id: 9, name: "Созвон с пиццой", actual: false, author: "Витя Матильдов", date: "21.10.2023", topic: "Пиццаводство", materials: "url", 
+qa: [{id: 1, text: "blablalbla"}, {id:2, text: "damn so cool"}], feedback: [ {id: 1, text:"kirill"}, {id:2, text:"danya"}] },
+{id: 10, name: "Созвон с чиксами", actual: false, author: "Вика Матильдова", date: "21.10.2023", topic: "Животноводство", materials: "url", 
+qa: [{id: 1, text: "blablalbla"}, {id:2, text: "damn so cool"}], feedback: [ {id: 1, text:"kirill"}, {id:2, text:"danya"}] },
+{id: 11, name: "Созвон с пиццой", actual: false, author: "Витя Матильдов", date: "21.10.2023", topic: "Пиццаводство", materials: "url", 
 qa: [{id: 1, text: "blablalbla"}, {id:2, text: "damn so cool"}], feedback: [ {id: 1, text:"kirill"}, {id:2, text:"danya"}] },
 
 ];
 
 
 function ModeratorPage() {
+
+    const [activeEvents, setActiveEvents] = useState(true);
+    const [showAddButton, setShowAddButton] = useState(true);
+
+    const toggleActiveEvents = () => {
+        setActiveEvents(!activeEvents);
+    };
+
+    const filteredMeetings = MEETINGS.filter((meeting) => meeting.actual === activeEvents);
+
     return ( 
         <>
             <>
@@ -60,23 +76,68 @@ function ModeratorPage() {
         <a style={{paddingLeft: '8%', paddingRight: '25%', paddingTop: '1%', fontSize:'36px', fontWeight: '600'}}>Редактирование событий</a>
 
         <div style={meetingsStyle}>
-            
-            <div style={{":hover": meetingDivHoverStyle }}>
-                {MEETINGS.map((meeting) => (
-                    <div key={meeting.id} style={{ ...meetingDivStyle }}>
-                    <a href={meeting.materials} style={{textDecoration: 'none', fontSize: '22px'}}>{meeting.name} {' '}</a>
-                     - <a style={{fontSize: '22px'}}>{meeting.topic}</a>
-                    <a style={{fontSize: '22px'}}>{meeting.date} , {meeting.author}</a>
-                    </div>
-                ))}
-            </div> 
-              
+        <div>
+          <a
+            style={{ 
+              textDecoration: activeEvents ? 'none' : 'none',
+              fontSize: '18px',
+              cursor: 'pointer',
+              fontWeight: activeEvents ? 'bold' : 'normal' }}
+            onClick={toggleActiveEvents}
+          >
+            Активные
+          </a>{' / '}
+          <a
+            style={{ 
+              textDecoration: activeEvents ? 'none' : 'none',
+              fontSize: '18px',
+              cursor: 'pointer',
+              fontWeight: activeEvents ? 'normal' : 'bold' }}
+            onClick={toggleActiveEvents}
+          >
+            Завершенные
+          </a>
         </div>
+
+        <div style={{ ":hover": meetingDivHoverStyle }}>
+          {filteredMeetings.map((meeting) => (
+            <div key={meeting.id} style={{ ...meetingDivStyle }}>
+              <a
+                href={meeting.materials}
+                style={{
+                  textDecoration: 'none',
+                  fontSize: '22px'
+                }}
+              >
+                {meeting.name} {' '}
+              </a>
+              - 
+              <a style={{ fontSize: '22px' }}>{meeting.topic}</a>
+              <a style={{ fontSize: '22px' }}>{meeting.date}, {meeting.author}</a>
+            </div>
+          ))}
+        </div>
+
+        {activeEvents && showAddButton && (
+        <div style={buttonAddStyle}>
+          <a>+Добавить</a>{/*TODO: сделать модальное окно чтобы добавлять */}
+        </div>
+      )}
+      </div>
+
+        
+
     </div>
         </>
         </>
      );
 }
+
+const buttonAddStyle = {
+    paddingTop: '15px',
+    fontWeight: 'bold',
+    cursor: 'pointer'
+};
 
 const meetingDivStyle = {
     backgroundColor: 'rgba(191, 191, 191, 0.2',
